fd12f1f572a83acb33637c3ab1d7388f1dc340cd
Edgar Morales <edmorales94@gmail.com>
2020-03-27 20:55:06 -0400

    Added the path library to render the html files

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const path = require("path");
 const port =  process.env.PORT || "8000";
 const app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res)=>{
-    res.status(200).send("Connected")
+    res.status(200).render("index")
 });
 
 app.listen(port, ()=>{
