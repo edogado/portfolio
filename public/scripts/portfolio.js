ba4394c93c62cbf9d3b8abf199ccf162217cec42
Edgar Morales <edmorales94@gmail.com>
2020-03-29 10:11:20 -0400

    deleted navbar code

--- public/scripts/portfolio.js
+++
@@ -1,9 +0,0 @@
-let mainNav = document.getElementById("js-menu");
-let navBarToggle = document.getElementById("js-navbar-toggle");
-
-
-document.addEventListener("DOMContentLoaded", ()=>{
-   navBarToggle.addEventListener("click",()=>{
-       mainNav.classList.toggle('active');
-   } );
-});
