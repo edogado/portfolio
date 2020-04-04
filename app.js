4c830c9c5ccd26fa8381015676acb5e0e042e854
Edgar Morales <edmorales94@gmail.com>
2020-04-03 22:30:32 -0400

    added the code to handle the email from the contact form

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const nodeMailer = require("nodemailer");
 const path = require("path");
 const port = "8000";
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 // noinspection JSUnresolvedFunction
 app.get("/", (req, res)=>{
@@ -11,7 +15,24 @@ app.get("/", (req, res)=>{
 });
 
 app.post("/messageSent", (req, res)=>{
-   res.send("You sent message");
+
+    let transporter = nodeMailer.createTransport({
+        host: "smtp.hostinger.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: 'website@edgarmorales.com', // generated ethereal user
+            pass: 'nacional319mx' // generated ethereal password
+        }
+    });
+
+    transporter.sendMail({
+        from: `${req.body.contactName} <website@edgarmorales.com>`, // sender address
+        to: "admin@edgarmorales.com", // list of receivers
+        subject: `${req.body.subject}`, // Subject line
+        text: `${req.body.contactMessage}\n\n${req.body.contactEmail}`
+    });
+    res.redirect("/");
 });
 
 app.listen(port, ()=>{
